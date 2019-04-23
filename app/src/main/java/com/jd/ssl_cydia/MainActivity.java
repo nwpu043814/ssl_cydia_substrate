ccc5c2a77bd36d2b79557fb17d8f8d97094d854c
lianh_mac_7F <lianh@jumei.com>
2019-04-23 16:56:43 +0800

    hook imei.

--- app/src/main/java/com/jd/ssl_cydia/MainActivity.java
+++ app/src/main/java/com/jd/ssl_cydia/MainActivity.java
@@ -2,12 +2,28 @@ package com.jd.ssl_cydia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    @BindView(R.id.imei)
+    TextView imei;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        imei.setText(getIMEI());
+    }
+
+    private String getIMEI(){
+        TelephonyManager tm = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
+        return tm.getDeviceId();
     }
 }
